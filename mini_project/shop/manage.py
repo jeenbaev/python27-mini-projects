317c7e35a93bdb3f15857ae4aa7476cc50568081
Bekbol <jeenbaev.bekbol@gmail.com>
2023-03-01 10:59:09 +0600

    shop done

--- mini_project/shop/manage.py
+++ mini_project/shop/manage.py
@@ -4,12 +4,15 @@
 # update - обновлять
 # delete - удаление
 
-from products import create, read
+from products import create, read, delete, update
 
 while True:
-    oper = input("c/r/u/d:")
+    oper = input("c/r/u/d: ")
     if oper == 'c':
         create()
     elif oper == 'r':
         read()
-        
+    elif oper == 'u':
+        update()
+    elif oper == 'd':
+        delete()
